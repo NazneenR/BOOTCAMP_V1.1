8d9e6d56510c5094350d34de34b08ab0d55a9121
NazneenRupawalla <nazneenr@thoughtworks.com>
2014-01-25 23:46:21 +0530

    Initial Commit.

---
+++ src/com/example/BOOTCAMP_1_1V/ShoppingApplication.java
@@ -0,0 +1,15 @@
+package com.example.BOOTCAMP_1_1V;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingApplication extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
