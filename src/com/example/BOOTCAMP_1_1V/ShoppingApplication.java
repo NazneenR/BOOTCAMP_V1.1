7da3e19619274176b7f23f1e05ff2c1fc2c26d0f
NazneenRupawalla <nazneenr@thoughtworks.com>
2014-01-30 00:02:06 +0530

    Implemented the simple list adapter.

--- src/com/example/BOOTCAMP_1_1V/ShoppingApplication.java
+++ src/com/example/BOOTCAMP_1_1V/ShoppingApplication.java
@@ -1,17 +1,14 @@
 package com.example.BOOTCAMP_1_1V;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 import com.example.Model.Product;
 import com.example.Repository.ProductRepository;
 
 import java.util.List;
 
-public class ShoppingApplication extends Activity {
+public class ShoppingApplication extends ListActivity {
     /**
      * Called when the activity is first created.
      */
@@ -21,18 +18,12 @@ public class ShoppingApplication extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setContentView(com.example.BOOTCAMP_1_1V.R.layout.main);
         productRepository = new ProductRepository();
-        LinearLayout layout = (LinearLayout) findViewById(R.id.listofitems);
-        List<Product> products  = productRepository.getProducts();
-        LayoutInflater layoutInflater = getLayoutInflater();
-        for (Product product : products) {
-            View view = layoutInflater.inflate(R.layout.product, null);
-            TextView titleView = (TextView)view.findViewById(R.id.title);
-            titleView.setText(product.getTitle());
-            TextView descriptionView = (TextView)view.findViewById(R.id.description);
-            descriptionView.setText(product.getDescription());
-            layout.addView(view);
-        }
+        List<Product> products = productRepository.getProducts();
+        ArrayAdapter<Product> adapter = new ArrayAdapter<Product>(this,
+                android.R.layout.simple_list_item_1, products);
+        setListAdapter(adapter);
     }
 }
