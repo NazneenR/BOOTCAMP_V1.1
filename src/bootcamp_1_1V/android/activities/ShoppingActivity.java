ace31543e312d97ee2f2ee2072b92696627b776a
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2014-02-05 05:26:36 +0530

    Abhinav: Response is loaded asynchronously. Images are still loaded in main thread.

--- src/bootcamp_1_1V/android/activities/ShoppingActivity.java
+++ src/bootcamp_1_1V/android/activities/ShoppingActivity.java
@@ -1,6 +1,7 @@
 package bootcamp_1_1V.android.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.GridView;
 
 import java.util.ArrayList;
 
+import androidplugins.Callback;
 import bootcamp_1_1V.android.R;
 import bootcamp_1_1V.android.adapters.ShoppingItemsListAdapter;
 import bootcamp_1_1V.android.models.Product;
@@ -30,21 +32,35 @@ public class ShoppingActivity extends Activity {
         setContentView(R.layout.main);
         final GridView gridView = (GridView) findViewById(R.id.grid_view);
 		ProductRepository productRepository = new ProductRepository();
-		ArrayList<Product> products = productRepository.getProducts();
+		final ProgressDialog progressDialog = ProgressDialog.show(this, "", "Loading...", true, true);
+		productRepository.getProducts(productsCallback(gridView, progressDialog));
 
-        gridView.setAdapter(new ShoppingItemsListAdapter(this, products));
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-
-                Product product = (Product) adapterView.getAdapter().getItem(position);
-                Intent intent = new Intent(getApplicationContext(), ProductDetailsActivity.class);
-                intent.putExtra(TITLE_KEY, product.getTitle());
-                intent.putExtra(DESCRIPTION_KEY, product.getDescription());
-                intent.putExtra(IMAGE_URL_KEY, product.getImageUrl());
-                startActivity(intent);
-            }
-        });
     }
+
+	private Callback<ArrayList<Product>> productsCallback(final GridView gridView, final ProgressDialog progressDialog) {
+		return new Callback<ArrayList<Product>>() {
+			@Override
+			public void execute(ArrayList<Product> products) {
+				renderProducts(gridView, products);
+				progressDialog.dismiss();
+			}
+		};
+	}
+
+	private void renderProducts(GridView gridView, ArrayList<Product> products) {
+		gridView.setAdapter(new ShoppingItemsListAdapter(this, products));
+
+		gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+				Product product = (Product) adapterView.getAdapter().getItem(position);
+				Intent intent = new Intent(getApplicationContext(), ProductDetailsActivity.class);
+				intent.putExtra(TITLE_KEY, product.getTitle());
+				intent.putExtra(DESCRIPTION_KEY, product.getDescription());
+				intent.putExtra(IMAGE_URL_KEY, product.getImageUrl());
+				startActivity(intent);
+			}
+		});
+	}
 }
--- src/bootcamp_1_1V/android/repositories/ProductRepository.java
+++ src/bootcamp_1_1V/android/repositories/ProductRepository.java
@@ -1,16 +1,24 @@
 package bootcamp_1_1V.android.repositories;
 
+import androidplugins.Callback;
+import androidplugins.contentfetcher.ContentFetcher;
 import bootcamp_1_1V.android.models.Product;
-import bootcamp_1_1V.android.services.ContentFetcher;
 import bootcamp_1_1V.android.services.ProductsParser;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ProductRepository {
 
-    public ArrayList<Product> getProducts() {
-        String strJSONData = new ContentFetcher().fetchResponse("http://xplorationstudio.com/sample_images/products_json.json");
-		return new ProductsParser().parseProducts(strJSONData);
+    public void getProducts(Callback<ArrayList<Product>> productsCallback) {
+        new ContentFetcher(responseCallback(productsCallback), "GET").execute("http://xplorationstudio.com/sample_images/products_json.json");
     }
+
+	private Callback<String> responseCallback(final Callback<ArrayList<Product>> productsCallback) {
+		return new Callback<String>() {
+			@Override
+			public void execute(String strJSONData) {
+				productsCallback.execute(new ProductsParser().parseProducts(strJSONData));
+			}
+		};
+	}
 }
